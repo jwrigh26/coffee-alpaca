a9149522b305d5715f5356ef034b35b45aa039c2
Justin Wright <jwright@swipeclock.com>
2024-01-24 11:57:07 -0700

    Added test script

---
+++ public/scripts/helloWorld.js
@@ -0,0 +1,11 @@
+// Ensure sc_user_employeeGuid is defined
+const sc_user_employeeGuid = window.sc_user_employeeGuid;
+
+// Construct the message
+const message = `Hello World ${sc_user_employeeGuid}`;
+
+// Print to console
+console.log(message);
+
+// Show an alert
+alert(message);
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/test-script': {
+        target: 'http://localhost:5173/scripts/helloWorld.js',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/test-script/, '')
+      }
+    }
+  }
 })
